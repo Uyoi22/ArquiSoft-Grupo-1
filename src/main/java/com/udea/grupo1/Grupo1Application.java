dd531fb43df698d8f3a1c7b1213ebfeca0b30761
Ana Granada <ana.granada1@udea.edu.co>
2025-12-05 12:05:28 -0500

    Refactors clima search endpoint and adds JPA

--- src/main/java/com/udea/grupo1/Grupo1Application.java
+++ src/main/java/com/udea/grupo1/Grupo1Application.java
@@ -2,8 +2,19 @@ package com.udea.grupo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
+})
+@ComponentScan(basePackages = "com.udea.grupo1",
+    excludeFilters = @ComponentScan.Filter(
+        type = FilterType.REGEX,
+        pattern = "com\\.udea\\.grupo1\\.controller\\.v1\\..*|com\\.udea\\.grupo1\\.service\\.WeatherService|com\\.udea\\.grupo1\\.repository\\.(City|Weather)Repository"
+    )
+)
 public class Grupo1Application {
 
 	public static void main(String[] args) {
